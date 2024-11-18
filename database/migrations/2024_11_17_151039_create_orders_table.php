f0e0276286ed2c114a9cae72e080e675771e0c40
Miguel <tecnologia1@zcmayoristas.com>
2024-11-18 17:13:25 -0500

    Ajustes

--- database/migrations/2024_11_17_151039_create_orders_table.php
+++ database/migrations/2024_11_17_151039_create_orders_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamp('order_date')->useCurrent();
-            $table->decimal('tax_rate', 5, 2)->default(18);  // Columna para la tasa de impuestos
-            $table->decimal('discount', 10, 2)->default(0);  // Columna para descuentos
+            $table->decimal('tax_rate', 5, 2)->default(18);
+            $table->decimal('discount', 10, 2)->default(0);
             $table->decimal('total_amount', 10, 2)->storedAs('(SELECT (SUM(quantity * unit_price) * (1 + tax_rate / 100) - discount) FROM order_details WHERE order_details.order_id = orders.order_id)');
             $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
